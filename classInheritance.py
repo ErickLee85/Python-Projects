7383bef6d5856d9eae9cbf4eb177f52848032d79
ErickLee85 <erickcrowne85@gmail.com>
2022-03-11 14:18:47 -0600

    classInheritance.py
    Added #Comments

--- classInheritance.py
+++ classInheritance.py
@@ -1,15 +1,15 @@
-class Movie:
+class Movie: #Creating a parent class "Movie" with default attributes
     cast = ('')
     producer = ' '
     writer = ' '
     base_budget = 1,000,000
 
-class Action(Movie):
+class Action(Movie): #Child class for "Action" genre of movies with unique attributes
     stunt_doubles = ('')
     exotic_locations = {''}
     safety_team = 'SafeSets'
 
-class History(Movie):
+class History(Movie): #Child class for "History" genre of movies with unique attributes
     historians = ('')
     archive = ' '
     scientists = ('')
