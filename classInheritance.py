3804cd1aad78a62f295d594684ca4c0c30e30b1d
ErickLee85 <erickcrowne85@gmail.com>
2022-03-11 14:13:12 -0600

    Create classInheritance.py
    Demonstrating class inheritance from the Parent Movie class to its children.

---
+++ classInheritance.py
@@ -0,0 +1,19 @@
+class Movie:
+    cast = ('')
+    producer = ' '
+    writer = ' '
+    base_budget = 1,000,000
+
+class Action(Movie):
+    stunt_doubles = ('')
+    exotic_locations = {''}
+    safety_team = 'SafeSets'
+
+class History(Movie):
+    historians = ('')
+    archive = ' '
+    scientists = ('')
+    
+    
+    
+    
