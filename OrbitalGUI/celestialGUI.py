d50f43fedfdd9ef5c72791364cd135d18dc40d48
ErickLee85 <erickcrowne85@gmail.com>
2022-03-23 20:49:20 -0500

    added planetary au list

--- OrbitalGUI/celestialGUI.py
+++ OrbitalGUI/celestialGUI.py
@@ -1,5 +1,7 @@
 from tkinter import *
 import math
+import os
+from tkinter import messagebox
 
 root = Tk()
 root.minsize(400,275)
@@ -17,7 +19,16 @@ def orbitalTime():
     
     
     
-    
+def msg():
+   messagebox.showinfo("Planets", "Mercury - 0.39\n"
+                                    "Venus - 0.72\n"
+                                   "Earth - 1\n"
+                                   "Mars - 1.52\n"
+                                   "Jupiter - 5.20\n"
+                                   "Saturn - 9.54\n"
+                                   "Uranus - 19.20\n"
+                                   "Neptune - 30.06\n"
+                                   "Pluto - 39.5")
     
 
 bg = PhotoImage(file="Image/spaceimage1.png")
@@ -31,7 +42,7 @@ my_canvas.create_image(0,0, image=bg)
 #creating transparent text with image as bg
 my_canvas.create_text(200,60,text="Enter the distance of your planet in AU(Astronomical Units).\n                            Example: Jupiters' AU is 5.20", fill="white")
                
-theEntry = Entry(width=25) #be sure to script widgets above canvas script or program won't find them.
+theEntry = Entry(width=50,) #be sure to script widgets above canvas script or program won't find them.
 theEntry.pack()
 theEntry_window=my_canvas.create_window(200,90, window=theEntry)
 
@@ -39,6 +50,10 @@ theEntry_window=my_canvas.create_window(200,90, window=theEntry)
 theButton = Button(pady=10, text="Calculate your planets year!",command=orbitalTime)
 theButton.pack()
 theButton_window=my_canvas.create_window(200,155,window=theButton)
+
+listButton = Button(pady=10, text="List of planets AU", command=msg)
+listButton.pack()
+button_window=my_canvas.create_window(200,250,window=listButton)
 
 root.mainloop()
 
